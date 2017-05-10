1d9bffd68f38d1cff91e8031cdadca7f32fdf8bf
Dustin Quam <dustinquam@me.com>
2017-05-10 14:42:25 -0500

    updated ignore file and removed npm debug log

---
+++ mysite/code/extensions/SiteConfigExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+class SiteConfigExtension extends DataExtension {
+
+	private static $db = array(
+		'GoogleAnalyticsID' => 'Text',
+	);
+
+	private static $has_one = array(
+		
+	);
+
+	private static $defaults = array(
+	
+	);
+
+	public function updateCMSFields(FieldList $fields) {
+
+		$fields->addFieldToTab('Root.Main', new TextField('GoogleAnalyticsID', 'Google Analytics ID (UA-XXXXX-X)'));
+
+
+		return $fields;
+	}
+
+}
+class SiteConfigExtensionPage_Controller extends Page_Controller {
+
+	public function init() {
+		parent::init();
+	}
+
+}
