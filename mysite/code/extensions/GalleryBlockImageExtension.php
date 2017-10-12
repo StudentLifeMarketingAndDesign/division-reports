66295e8b35006d886b717d92f5acc35c42cca512
Dustin Quam <quamsta@gmail.com>
2017-10-12 15:07:52 -0500

    removed test field

--- mysite/code/extensions/GalleryBlockImageExtension.php
+++ mysite/code/extensions/GalleryBlockImageExtension.php
@@ -3,7 +3,7 @@
 class GalleryBlockImageExtension extends DataExtension
 {
 	private static $db = array(
-		'Test' => 'Text'
+	
 	);
     private static $belongs_many_many = array(
         'GalleryBlock' => 'GalleryBlock'
